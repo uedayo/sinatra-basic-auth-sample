de46098479fef6a59d04605d3bf0f8d07501d616
Yoshinori Ueda <uedayo@gmail.com>
2013-10-19 23:33:55 +0900

    add basic auth

--- web.rb
+++ web.rb
@@ -1,5 +1,27 @@
+require 'rubygems'
 require 'sinatra'
 
+helpers do
+  def protect!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    username = ENV['BASIC_AUTH_USERNAME']
+    password = ENV['BASIC_AUTH_PASSWORD']
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [username, password]
+  end
+end
+
 get '/' do
-  "Hello, world"
+  erb :index
+end
+
+get '/protect' do
+  protect!
+  'Authorized!'
 end
